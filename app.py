bf00bd62bdad580f50aff812576a6532fe05822a
singhrau0 <singhrau@gmail.com>
2024-05-29 20:43:38 +0530

    vrdsg

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
+import sqlite3
 
 app = Flask(__name__)
 
@@ -6,6 +7,30 @@ app = Flask(__name__)
 @app.route('/')
 def homepage():
     return render_template("home.html")
+
+@app.route('/contact',methods = ["GET","POST"])
+def contactus():
+    if request.method=='POST':
+        name = request.form.get("name")
+        email = request.form.get("email")
+        country = request.form.get("country")
+        state = request.form.get("state")
+        message = request.form.get("message")
+        print(name,email,country,state,message)
+        conn = sqlite3.connect('contactus.db')
+        cur = conn.cursor()
+        cur.execute(f'''
+        INSERT INTO CONTACT VALUES(
+                    "{name}","{email}",
+                    "{country}","{state}",
+                    "{message}"
+        )
+        ''')
+        conn.commit()
+        return render_template("message.html")
+    else:
+        return render_template('contactus.html')
+
 
 
 
